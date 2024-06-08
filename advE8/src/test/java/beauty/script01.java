cf8f988fe7efec39914a3ed9a6f63f83a29f8850
91823 <91823@DESKTOP-48A98GS>
2024-06-08 20:20:57 +0530

    i have write a new script in beauty

---
+++ advE8/src/test/java/beauty/script01.java
@@ -0,0 +1,15 @@
+package beauty;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class script01 {
+	@Test
+	
+	
+	public void script1()
+	{
+		Reporter.log("script is written",true);
+	}
+
+}
